1beb101b0c35271b888f0a9624745942aa7102bc
Henning Thielemann <git@henning-thielemann.de>
2018-02-15 16:46:51 +0100

    ST.Vault: instance Semigroup

--- src/Data/Vault/ST/ST.hs
+++ src/Data/Vault/ST/ST.hs
@@ -13,6 +13,7 @@ module Data.Vault.ST.LAZINESS (
     ) where
 
 import Data.Monoid (Monoid(..))
+import Data.Semigroup (Semigroup((<>)))
 import Prelude hiding (lookup)
 import Control.Applicative hiding (empty)
 import Control.Monad.ST
@@ -37,6 +38,9 @@ import Data.Unique.Really
 {-----------------------------------------------------------------------------
     Vault
 ------------------------------------------------------------------------------}
+
+instance Semigroup (Vault s) where
+    (<>) = union
 
 instance Monoid (Vault s) where
     mempty = empty
