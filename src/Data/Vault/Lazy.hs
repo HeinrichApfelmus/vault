753325a36e56e2f714c1f91eb953abbf2303ab79
mrkkrp <markkarpov92@gmail.com>
2018-05-23 16:08:35 +0700

    Do the same for ‘IO.hs’

--- src/Data/Vault/Lazy.hs
+++ src/Data/Vault/Lazy.hs
@@ -3,4 +3,4 @@
 -- | A persistent store for values of arbitrary types.
 --
 -- The 'Vault' type in this module is strict in the keys but lazy in the values.
-#include "IO.hs"
+#include "IO.h"
--- src/Data/Vault/Strict.hs
+++ src/Data/Vault/Strict.hs
@@ -3,4 +3,4 @@
 -- | A persistent store for values of arbitrary types.
 --
 -- The 'Vault' type in this module is strict in both keys and values.
-#include "IO.hs"
+#include "IO.h"
